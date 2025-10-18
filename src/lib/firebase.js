7e1423c9a4686b53a26e9c051078e2e8fc3e2f72
Chris Paris <cparis@xp-electric.com>
2025-10-17 17:32:13 -0700

    Remove duplicate data inside firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -13,20 +13,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-// src/lib/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyADn8XumdQHUB6eMVtjkxahqY_kFAtxCC8",  // Replace with your full config from console
-  authDomain: "snappdraft.firebaseapp.com",
-  projectId: "snappdraft",
-  storageBucket: "snappdraft.firebasestorage.app",
-  messagingSenderId: "88069653855",
-  appId: "1:88069653855:web:1f072dbba14c46b8c3a2ea",
-  measurementId: "G-PGKH45E1T1"
-};
-
-const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
